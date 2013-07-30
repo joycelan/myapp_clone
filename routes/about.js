42193d9e31f6aab0a4f1dd271846fe7f1da00e15
Joyce Lan <joycelan1124@gmail.com>
2013-07-30 11:52:40 +0800

    update about us

--- routes/about.js
+++ routes/about.js
@@ -3,8 +3,8 @@
  * GET aboutus listing.
  */
 exports.aboutus = function(req, res){
-  res.render('aboutus', { title: 'Honpa vision' });
+  res.render('aboutus', { title: 'Honpac vision' });
 };
 exports.aboutus_en = function(req, res){
-  res.render('aboutus', { title: 'Honpa vision' });
+  res.render('aboutus', { title: 'Honpac vision' });
 };
